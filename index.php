4342f89da691bb66f05e033929bc855b586347c4
sergio <sergiojosel.j@gmail.com>
2020-07-20 20:39:34 -0500

    adminlte a mvc modulo usuario y categoria lista

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "controladores/plantilla.controlador.php";
+require_once "controladores/usuarios.controlador.php";
+require_once "controladores/categorias.controlador.php";
+
+
+require_once "modelos/usuarios.modelo.php";
+require_once "modelos/categorias.modelo.php";
+
+
+
+
+$plantilla = new ControladorPlantilla();
+$plantilla -> ctrPlantilla();
