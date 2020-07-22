55f1a043c12ee341ab0b109a8053c92f75fb2096
sergio <sergiojosel.j@gmail.com>
2020-07-21 22:22:56 -0500

    modelo y controlador modelos

---
+++ controladores/modelos.controlador.php
@@ -0,0 +1,181 @@
+<?php
+
+class ControladorModelos{
+
+	/*=============================================
+	CREAR MODELOS
+	=============================================*/
+
+	static public function ctrCrearModelo(){
+
+		if(isset($_POST["nuevaMarca"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevaMarca"])){
+
+				$tabla = "marca";
+
+				$datos = $_POST["nuevaMarca"];
+
+				$respuesta = ModeloMarcas::mdlIngresarMarca($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "La nueva MARCA ha sido guardada correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "marcas";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡La marca no puede ir vacía o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "marcas";
+
+							}
+						})
+
+			  	</script>';
+
+			}
+
+		}
+
+	}
+
+	/*=============================================
+	MOSTRAR MODELOS
+	=============================================*/
+
+	static public function ctrMostrarModelo($item, $valor){
+
+		$tabla = "modelo";
+
+		$respuesta = ModeloModelos::mdlMostrarModelos($tabla, $item, $valor);
+
+		return $respuesta;
+	
+	}
+
+	/*=============================================
+	EDITAR MARCA
+	=============================================*/
+
+	static public function ctrEditarModelo(){
+
+		if(isset($_POST["editarMarca"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["editarMarca"])){
+
+				$tabla = "marca";
+
+				$datos = array("marca"=>$_POST["editarMarca"],
+							   "id"=>$_POST["idMarca"]);
+
+				$respuesta = ModeloModelos::mdlEditarModelo($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "La  MARCA ha sido cambiada correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "marcas";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡La MARCA no puede ir vacía o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "marcas";
+
+							}
+						})
+
+			  	</script>';
+
+			}
+
+		}
+
+	}
+
+	/*=============================================
+	BORRAR MARCA
+	=============================================*/
+
+	static public function ctrBorrarModelo(){
+
+		if(isset($_GET["idMarca"])){
+
+			$tabla ="marca";
+			$datos = $_GET["idMarca"];
+
+			$respuesta = ModeloModelos::mdlBorrarModelo($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "La marca ha sido borrada correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "marcas";
+
+									}
+								})
+
+					</script>';
+			}
+		}
+		
+	}
+}
---
+++ vistas/modulos/modelos.php
@@ -0,0 +1,253 @@
+<div class="content-wrapper">
+
+  <section class="content-header">
+    
+    <h1>
+      
+      Administrar modelo
+    
+    </h1>
+
+    <ol class="breadcrumb">
+      
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Administrar modelo</li>
+    
+    </ol>
+
+  </section>
+
+  <section class="content">
+
+    <div class="box">
+
+      <div class="box-header with-border">
+  
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarModelo">
+          
+          Agregar modelo
+
+        </button>
+
+      </div>
+
+      <div class="box-body">
+        
+       <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
+         
+        <thead>
+         
+         <tr>
+           
+           <th style="width:10px">#</th>
+           <th>Categoria</th>
+           <th>Marca</th>
+           <th>Modelo</th>
+
+         </tr> 
+
+        </thead>
+
+        <tbody>
+
+        <?php
+
+          $item = null;
+          $valor = null;
+
+          $modelo = ControladorModelos::ctrMostrarModelo($item, $valor);
+
+          foreach ($modelo as $key => $value) {
+           
+            echo ' <tr>
+
+                    <td>'.($key+1).'</td>
+
+                    <td class="text-uppercase">'.$value["categoria"].'</td>
+                    <td class="text-uppercase">'.$value["marca"].'</td>
+                    <td class="text-uppercase">'.$value["descripcion"].'</td>
+
+                    <td>
+
+                      <div class="btn-group">
+                          
+                        <button class="btn btn-warning btnEditarMarca" idMarca="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarMarca"><i class="fa fa-pencil"></i></button>
+
+                        <button class="btn btn-danger btnEliminarMarca" idMarca="'.$value["id"].'"><i class="fa fa-times"></i></button>
+
+                      </div>  
+
+                    </td>
+
+                  </tr>';
+          }
+
+        ?>
+
+        </tbody>
+
+       </table>
+
+      </div>
+
+    </div>
+
+  </section>
+
+</div>
+
+<!--=====================================
+MODAL AGREGAR CATEGORÍA
+======================================-->
+
+<div id="modalAgregarMarca" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Agregar marca</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevaMarca" placeholder="Ingresar marca" required>
+
+              </div>
+
+            </div>
+  
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar marca</button>
+
+        </div>
+
+        <?php
+
+          $crearMarca = new ControladorMarcas();
+          $crearMarca -> ctrCrearMarca();
+
+        ?>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+<!--=====================================
+MODAL EDITAR CATEGORÍA
+======================================-->
+
+<div id="modalEditarMarca" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Editar marca</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarMarca" id="editarMarca" required>
+
+                 <input type="hidden"  name="idMarca" id="idMarca" required>
+
+              </div>
+
+            </div>
+  
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar cambios</button>
+
+        </div>
+
+     
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+
+
