d3132aa485aa69b08c0ee4da9b50cd631f493f9b
sergio jara <sergiojosel.j@gmail.com>
2021-02-24 21:32:42 -0500

    agregando otro fondo y estilo al login

--- vistas/modulos/login.php
+++ vistas/modulos/login.php
@@ -1,17 +1,17 @@
 <div id="back"></div>
 
 <div class="login-box">
-  
-  <div class="login-logo">
 
-  <!--  <img src="vistas/img/plantilla/avatar.png" class="img-responsive" style="padding:30px 100px 0px 100px  ">-->
 
-  </div>
 
   <div class="login-box-body">
 
     <p class="login-box-msg"><b>Ingresar al sistema</b></p>
+    <div class="login-logo">
 
+      <img src="vistas/img/plantilla/logo-07.jpg" class="img-responsive" style="padding:30px 100px 0px 100px  ">
+
+    </div>
     <form method="post">
 
       <div class="form-group has-feedback">
@@ -25,24 +25,24 @@
 
         <input type="password" class="form-control" placeholder="Contraseña" name="ingPassword" required>
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      
+
       </div>
 
       <div class="row ">
-       
+
         <div class="col-xs-4 ">
 
           <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
-        
+
         </div>
 
       </div>
 
       <?php
 
-        $login = new ControladorUsuarios();
-        $login -> ctrIngresoUsuario();
-        
+      $login = new ControladorUsuarios();
+      $login->ctrIngresoUsuario();
+
       ?>
 
     </form>
