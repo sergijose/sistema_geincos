76fd45420caf2f7ac46c257ca257b373b2274c0c
sergio jara <sergiojosel.j@gmail.com>
2021-04-28 22:54:52 -0500

    diseñando modal de agregar detalle de producto cpu

--- vistas/modulos/productos-cpu.php
+++ vistas/modulos/productos-cpu.php
@@ -46,8 +46,8 @@ if ($_SESSION["perfil"] == "Visitante") {
 
         </button>
 
-     
-        
+
+
 
       </div>
 
@@ -62,7 +62,7 @@ if ($_SESSION["perfil"] == "Visitante") {
               <th style="width:10px">#</th>
               <th>Codigo</th>
               <th>Tipo Disco</th>
-               <th>Tamaño Disco</th> 
+              <th>Tamaño Disco</th>
               <th>Tipo Ram</th>
               <th>Tamaño Ram</th>
               <th>Procesador</th>
@@ -96,7 +96,7 @@ MODAL AGREGAR PRODUCTO
 
     <div class="modal-content">
 
-      <form role="form" method="post"  enctype="multipart/form-data" >
+      <form role="form" method="post" enctype="multipart/form-data">
 
         <!--=====================================
         CABEZA DEL MODAL
@@ -115,12 +115,12 @@ MODAL AGREGAR PRODUCTO
         ======================================-->
 
         <div class="modal-body">
-          
+
           <div class="box-body">
 
 
 
-            <!-- ENTRADA PARA SELECCIONAR MODELO -->
+            <!-- ENTRADA PARA SELECCIONAR CODIGO DE PRODUCTO DE LA CATEGORIA CPU -->
 
             <div class="form-group">
 
@@ -128,82 +128,21 @@ MODAL AGREGAR PRODUCTO
 
                 <span class="input-group-addon"><i class="fa fa-th"></i></span>
 
-                <select class="form-control input-lg" id="nuevoCodProductoCpu" name="nuevoCodProductoCpu" required>
+                <select class="form-control input-md" id="nuevoCodProductoCpu" name="nuevoCodProductoCpu" required>
 
                   <option value="">Seleccionar Codigo CPU</option>
 
                   <?php
 
-                 
+
                   $categoria = "CPU";
-                  
+
 
                   $codProducto = ControladorProductosCpu::ctrMostrarCodigoProductoCpu($categoria);
 
                   foreach ($codProducto as $key => $value) {
 
-                    echo '<option value="' . $value["id"] . '">' . $value["cod_producto"] .'</option>';
-                  }
-                 // var_dump($codProducto);
-                  ?>
-
-                </select>
-
-              </div>
-
-            </div>
-
-            <!-- ENTRADA PARA EL CÓDIGO -->
-
-            <div class="form-group">
-
-              <div class="input-group">
-
-                <span class="input-group-addon"><i class="fa fa-code"></i></span>
-
-                <input type="text" class="form-control input-lg" id="nuevoCodigo" name="nuevoCodigo" placeholder="Ingrese Codigo" required>
-
-              </div>
-
-            </div>
-            <!-- ENTRADA PARA EL NUMERO DE SERIE -->
-
-            <div class="form-group">
-
-              <div class="input-group">
-
-                <span class="input-group-addon"><i class="fa fa-code"></i></span>
-
-                <input type="text" class="form-control input-lg" id="nuevoNumSerie" name="nuevoNumSerie" placeholder="Ingrese numero de serie" >
-
-              </div>
-
-            </div>
-            <!-- ENTRADA PARA EL ESTADO -->
-
-            <div class="form-group">
-
-              <div class="input-group">
-
-                <span class="input-group-addon"><i class="fa fa-th"></i></span>
-
-                <select class="form-control input-lg" id="nuevoEstado" name="nuevoEstado" required>
-
-                  <option value="">Seleccionar estado del producto</option>
-
-                  <?php
-
-                  $item = null;
-                  $valor = null;
-                  $orden = "id";
-
-
-                  $estado = ControladorProductos::ctrMostrarEstadoProducto($item, $valor, $orden);
-
-                  foreach ($estado as $key => $value) {
-
-
-                    echo '<option value="' . $value["id"] . '">' . $value["descripcion"] . '</option>';
+                    echo '<option value="' . $value["id"] . '">' . $value["cod_producto"] . '</option>';
                   }
 
                   ?>
@@ -214,8 +153,7 @@ MODAL AGREGAR PRODUCTO
 
             </div>
 
-
-            <!-- ENTRADA PARA ESTADO DE PRESTAMO DEL PRODUCTO -->
+            <!-- ENTRADA PARA SELECCIONAR EL TIPO DE DISCO DURO -->
 
             <div class="form-group">
 
@@ -223,28 +161,135 @@ MODAL AGREGAR PRODUCTO
 
                 <span class="input-group-addon"><i class="fa fa-th"></i></span>
 
-                <select class="form-control input-lg" id="nuevoEstadoPrestamo" name="nuevoEstadoPrestamo" required>
+                <select class="form-control input-md" id="nuevoTipoDisco" name="nuevoTipoDisco" required>
 
-                  <option value="">Seleccionar estado del prestamo</option>
-                  <option value="DISPONIBLE">DISPONIBLE</option>
-                  <option value="OCUPADO">OCUPADO</option>
+                  <option value="">Seleccionar tipo de disco</option>
+                  <option value="MECANICO">MECANICO</option>
+                  <option value="SOLIDO">SOLIDO</option>
+
+
                 </select>
 
               </div>
 
             </div>
 
-  <!-- ENTRADA PARA IMAGEN CODIGO DE BARRAS DEL PRODUCTO -->
 
-  <div class="form-group">
 
-<div class="input-group">
-<!--<input type="file" name="barcode1">-->
-  <img id="barcode"  name="barcode" ></img>
- <!--</input>  -->
-</div> 
+            <!-- ENTRADA PARA CANTIDAD DE DISCO -->
 
-</div>
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-signal"></i></span>
+
+                <input type="number" class="form-control input-md" id="nuevaCantDisco" name="nuevaCantDisco" placeholder="Ingrese GB de disco duro" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SELECCIONAR EL TIPO DE MEMORIA RAM -->
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-th"></i></span>
+
+                <select class="form-control input-md" id="nuevoTipoRam" name="nuevoTipoRam" required>
+
+                  <option value="">Seleccionar tipo de Memoria Ram</option>
+                  <option value="DR4">DR4</option>
+                  <option value="DR3">DR3</option>
+                  <option value="DR2">DR2</option>
+                  <option value="DR">DR</option>
+
+
+                </select>
+
+              </div>
+
+            </div>
+
+
+
+            <!-- ENTRADA PARA CANTIDAD DE MEMORIA RAM -->
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-signal"></i></span>
+
+                <input type="number" class="form-control input-md" id="nuevaCantRam" name="nuevaCantRam" placeholder="Ingrese GB de Memoria Ram" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SELECCIONAR EL TIPO DE PROCESADOR-->
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-th"></i></span>
+
+                <select class="form-control input-md" id="nuevoProcesador" name="nuevoProcesador" required>
+
+                  <option value="">Seleccionar tipo de Procesador</option>
+                  <option value="DR4">i9</option>
+                  <option value="DR3">i7</option>
+                  <option value="DR2">i5</option>
+                  <option value="DR">i3</option>
+                  <option value="DR">AMD</option>
+
+
+                </select>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SELECCIONAR EL SISTEMA OPERATIVO-->
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-th"></i></span>
+
+                <select class="form-control input-md" id="nuevoSistemaOperativo" name="nuevoSistemaOperativo" required>
+
+                  <option value="">Seleccionar Sistema Operativo</option>
+                  <option value="DR4">WIN 10</option>
+                  <option value="DR3">WIN 8</option>
+                  <option value="DR2">WIN 7</option>
+                  <option value="DR">LINUX</option>
+
+
+
+                </select>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LAS OBSERVACIONES-->
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+              <span class="input-group-addon"><i class="fa fa-pencil"></i></span>
+
+<textarea class="form-control" id="nuevaObservacion" name="nuevaObservacion" cols="5" rows="2" placeholder="escriba aqui si tiene alguna observacion de este producto"  required></textarea>
+
+              </div>
+
+            </div>
 
 
           </div>
@@ -258,7 +303,7 @@ MODAL AGREGAR PRODUCTO
 
           <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
 
-          <button type="submit" class="btn btn-primary">Guardar producto</button>
+          <button type="submit" class="btn btn-primary">Guardar Informacion</button>
 
         </div>
 
@@ -334,7 +379,7 @@ MODAL EDITAR PRODUCTO
 
                   foreach ($modelo as $key => $value) {
 
-                    echo '<option value="' . $value["id"] . '">' . $value["categoria"] ."| ".$value["marca"] ."| ".$value["descripcion"] . '</option>';
+                    echo '<option value="' . $value["id"] . '">' . $value["categoria"] . "| " . $value["marca"] . "| " . $value["descripcion"] . '</option>';
                   }
 
                   ?>
@@ -367,7 +412,7 @@ MODAL EDITAR PRODUCTO
 
                 <span class="input-group-addon"><i class="fa fa-code"></i></span>
 
-                <input type="text" class="form-control input-lg" id="editarNumSerie" name="editarNumSerie" placeholder="editar numero de serie" >
+                <input type="text" class="form-control input-lg" id="editarNumSerie" name="editarNumSerie" placeholder="editar numero de serie">
 
               </div>
 
